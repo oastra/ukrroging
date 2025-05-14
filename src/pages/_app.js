4d1dde247d89a2e9e2abb8a2ec980f7c35f5b9b0
Olha Chernysh <oastra555@gmail.com>
2025-05-14 19:11:52 +1000

    Google Analitics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,27 @@
-import "@/styles/globals.css";
+import Head from "next/head";
+import "../styles/globals.css"; // if you're using global styles
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-SBM45GGPYP"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-SBM45GGPYP');
+            `,
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
